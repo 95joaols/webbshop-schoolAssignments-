3b84b8ef439922b32526cb729eef4f2cac8b97ad
millbergm <83matmil@skola.boras.se>
2021-08-31 15:18:53 +0200

    commented new properties in src/entity/product.ts

--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,9 +1,9 @@
 export type Product = {
     Id: number,
     Name: string,
-    year: number,
-    genre: string,
-    rating: number,
+    // year: number,
+    // genre: string,
+    // rating: number,
     price: number,
     description: string,
     imageUrl: string
