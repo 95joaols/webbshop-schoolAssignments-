5bd53f337a928d50e502e9583cf19ee9ffb0ed98
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-09-01 16:00:33 +0200

    fix

--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,10 +1,10 @@
 export type Product = {
-    Id: number,
-    Name: string,
-    // year: number,
-    // genre: string,
-    // rating: number,
-    price: number,
-    description: string,
-    imageUrl: string
-}
+  Id: number;
+  Name: string;
+  year: number;
+  genre: string;
+  rating: number;
+  price: number;
+  description: string;
+  imageUrl: string;
+};
