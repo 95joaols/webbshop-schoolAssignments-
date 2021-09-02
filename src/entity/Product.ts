9008c93bb553bc84550303c9debabf506bc24db5
millbergm <83matmil@skola.boras.se>
2021-09-02 15:04:21 +0200

    ...

--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,3 +1,6 @@
+import React from "react";
+import * as products from '../../products.json';
+
 export type Product = {
     Id: number,
     Name: string,
@@ -7,4 +10,22 @@ export type Product = {
     price: number,
     description: string,
     imageUrl: string
+}
+
+export let allProducts: Product[];
+
+export function SaveProductsToLocalStorage() {
+    localStorage.setItem("allProducts", JSON.stringify(allProducts));
+}
+
+export function GetProductsFromLocalStorage() {
+    const productString = localStorage.getItem("allProducts");
+
+    if (!productString) {
+        allProducts = JSON.parse(products).map((product) => {
+            return new product(
+                product.
+            );
+        })
+    }
 }
