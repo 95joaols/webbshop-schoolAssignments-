a5a981fcbfbd8760e633efdc497c0bdb75b1774b
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-09-03 10:25:03 +0200

    tooltips

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { Product } from "../entity/Product";
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
@@ -7,6 +7,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import { AddProductToCart } from "./AddProductToCart";
 import { Link } from "react-router-dom";
+import { Tooltip, Zoom } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
@@ -31,48 +32,59 @@ const useStyles = makeStyles({
 
 interface props {
   product: Product;
+  timeToShow: number;
 }
 
-export const ProductCard: React.FC<props> = ({ product }) => {
+export const ProductCard: React.FC<props> = ({ product, timeToShow }) => {
+  const [show, setShow] = useState(false);
+  const timerId = setInterval(() => {
+    setShow(true);
+    clearInterval(timerId);
+  }, timeToShow);
+
   const classes = useStyles();
   const url = `/detail/${product.id}`;
 
   return (
-    <Card className={classes.root}>
-      <Link to={url} className={classes.link}>
-        <CardMedia
-          className={classes.media}
-          image={product.imageUrl}
-          title={product.name}
-        />
-      </Link>
-      <CardContent className={classes.Content}>
+    <Zoom in={show}>
+      <Card className={classes.root}>
         <Link to={url} className={classes.link}>
-          <Typography noWrap gutterBottom component="p">
-            {product.name}
-          </Typography>
-          <Typography variant="body2" component="p">
-            Year: {product.year}
-          </Typography>
-          <Typography variant="body2" component="p">
-            Genre: {product.genre}
-          </Typography>
-          <Typography gutterBottom variant="body2" component="p">
-            Rating: {product.rating}
-          </Typography>
+          <CardMedia
+            className={classes.media}
+            image={product.imageUrl}
+            title={product.name}
+          />
         </Link>
-      </CardContent>
-      <CardContent className={classes.Content}>
-        <Typography
-          gutterBottom
-          variant="body2"
-          color="textSecondary"
-          component="p"
-        >
-          {product.price}kr
-        </Typography>
-        <AddProductToCart product={product} />
-      </CardContent>
-    </Card>
+        <CardContent className={classes.Content}>
+          <Link to={url} className={classes.link}>
+            <Tooltip title={product.name} aria-label="add">
+              <Typography noWrap gutterBottom component="p">
+                {product.name}
+              </Typography>
+            </Tooltip>
+            <Typography variant="body2" component="p">
+              Year: {product.year}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Genre: {product.genre}
+            </Typography>
+            <Typography gutterBottom variant="body2" component="p">
+              Rating: {product.rating}
+            </Typography>
+          </Link>
+        </CardContent>
+        <CardContent className={classes.Content}>
+          <Typography
+            gutterBottom
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          >
+            {product.price}kr
+          </Typography>
+          <AddProductToCart product={product} />
+        </CardContent>
+      </Card>
+    </Zoom>
   );
 };
