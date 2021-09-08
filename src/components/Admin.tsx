dc1d062c47c23f198339753ead7657bdbde7be42
Andreas Gunnarsson <andreas.gunnarsson@gmail.com>
2021-09-08 17:55:57 +0200

    - Removed the separate modal component since it never worked correctly. Moved the modal back to Admin.tsx once again.. - Updated the style on the modal and the list.

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,22 +1,28 @@
 import { useState, useContext } from 'react';
 import { Product } from "../entity/Product";
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import { FormHelperText, FormControl, Input, InputLabel, List, ListItem, ListItemText, Button } from '@material-ui/core';
-import { AdminModal } from './AdminModal';
+import { List, ListItem, ListItemText, Button, TextField } from '@material-ui/core';
 import { ProductContext } from '../contexts/ProductContext';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import Modal from '@material-ui/core/Modal';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: '100%',
-      maxWidth: 360,
-      backgroundColor: '#d71f5f',
+      color: 'white',
+      backgroundColor: '#ffffff',
     },
     modal: {
+      display: 'flex',
+      flexDirection: 'column',
+      top: '1%',
+      left: '50%',
+      marginTop: '-100',
+      marginLeft: '-150',
       position: 'absolute',
-      width: 400,
-      backgroundColor: '#bf95d4',
-      border: '2px solid #000',
+      backgroundColor: '#ffffff',
+      borderRadius: '2rem',
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
     },
@@ -28,7 +34,7 @@ export const AdminComponent: React.FC = () => {
   const [ open, setOpen ] = useState(false);
   const [ selectedProduct, setSelectedProduct ] = useState<Product>({
     id: -551,
-    name: '',
+    name: 'test',
     year: -1,
     genre: '',
     rating: -1,
@@ -37,8 +43,14 @@ export const AdminComponent: React.FC = () => {
     imageUrl: ''
   });
   const { products, updateProduct, deleteProduct, addProduct } = useContext(ProductContext);
+  const { register, handleSubmit, watch, formState: { errors }, getValues, setValue, unregister } = useForm<Product>();
+
+  const onSubmit: SubmitHandler<Product> = (data) => {
+    console.log('SubmitHandler: ', data )
+  };
 
   const handleOpen = (product: Product) => {
+    unregister(['name', 'year', 'genre', 'rating', 'price', 'description', 'imageUrl']);
     setSelectedProduct(product);
     setOpen(true);
     console.log('handleOpen');
@@ -51,41 +63,75 @@ export const AdminComponent: React.FC = () => {
 
   const handleAddNew = () => {
     console.log('handleAddNew');
-    // console.log(products);                  // Debug.
-    // addProduct({
-    //   id: -1,
-    //   name: '',
-    //   year: -1,
-    //   genre: '',
-    //   rating: -1,
-    //   price: -1,
-    //   description: '',
-    //   imageUrl: ''
-    // });
+    unregister(['name', 'year', 'genre', 'rating', 'price', 'description', 'imageUrl']);
+    setSelectedProduct({
+      id: -1,
+      name: '',
+      year: -1,
+      genre: '',
+      rating: -1,
+      price: -1,
+      description: '',
+      imageUrl: ''
+    });
     setOpen(true);
-    // updateProduct({
-    //   id: 3,
-    //   name: '',
-    //   year: -1,
-    //   genre: '',
-    //   rating: -1,
-    //   price: -1,
-    //   description: '',
-    //   imageUrl: ''
-    // });
   };
+
+  const modalBodyEditProduct = (
+  <div className={classes.modal}>
+    <h1>Edit</h1>
+      { <form onSubmit={handleSubmit(onSubmit)} style={{display: "flex", flexDirection: "row"}}>
+          <div style={{display: "flex", flexDirection: "column"}}>
+            <TextField type="number" label="Id" defaultValue={selectedProduct.id} disabled />
+            <TextField type="text" label="Name" defaultValue={selectedProduct.name} {...register("name")} />
+            <TextField type="number" label="Year" defaultValue={selectedProduct.year} {...register("year")}/>
+            <TextField type="text" label="Genre" defaultValue={selectedProduct.genre} {...register("genre")} />
+          </div>
+          <div style={{display: "flex", flexDirection: "column"}}>
+            <TextField type="number" label="Rating" defaultValue={selectedProduct.rating} {...register("rating")} />
+            <TextField type="number" label="Price" defaultValue={selectedProduct.price} {...register("price")} />
+            <TextField type="text" label="Description" defaultValue={selectedProduct.description} {...register("description")} />
+            <TextField type="url" label="Image URL" defaultValue={selectedProduct.imageUrl} {...register("imageUrl")} />
+          </div>
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+      </form> }
+  </div>
+  );
+
+  const modalBodyNewProduct = (
+  <div className={classes.modal}>
+    <h1>Create new</h1>
+      { <form onSubmit={handleSubmit(onSubmit)} style={{display: "flex", flexDirection: "row"}}>
+          <div style={{display: "flex", flexDirection: "column"}}>
+            <TextField type="number" label="Id" disabled />
+            <TextField type="text" label="Name" {...register("name")} />
+            <TextField type="number" label="Year" {...register("year")}/>
+            <TextField type="text" label="Genre" {...register("genre")} />
+          </div>
+          <div style={{display: "flex", flexDirection: "column"}}>
+            <TextField type="number" label="Rating" {...register("rating")} />
+            <TextField type="number" label="Price" {...register("price")} />
+            <TextField type="text" label="Description" {...register("description")} />
+            <TextField type="url" label="Image URL" {...register("imageUrl")} />
+          </div>
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+      </form> }
+  </div>
+  );
 
   return (
     <div className={classes.root}>
       <List component="nav">
       { products.map((p) => (
-        <div key={p.id}>
+        <div key={p.id} style={{backgroundColor: "#3f51b5", marginTop: "1rem", borderRadius: "1rem"}}>
           <ListItem button onClick={() => handleOpen(p)}>
-            <ListItemText primary={p.id} />
             <ListItemText primary={p.name} />
             <ListItemText primary={p.year} />
             <ListItemText primary={p.genre} />
-            <ListItemText primary={p.description} />
             <ListItemText primary={p.price} />
           </ListItem>
             <Button onClick={() => handleDelete(p.id)}>Delete</Button>
@@ -93,7 +139,9 @@ export const AdminComponent: React.FC = () => {
       ))}
         <Button onClick={handleAddNew}>Add new</Button>
       </List>
-        <AdminModal isOpen={open} setOpenHook={setOpen} productForModal={selectedProduct} productToParent={setSelectedProduct} />
+      <Modal open={open} onClose={() => {setOpen(false)}}>
+        {selectedProduct.id >= 0 ? modalBodyEditProduct : modalBodyNewProduct}
+      </Modal>
     </div>
   );
 };
