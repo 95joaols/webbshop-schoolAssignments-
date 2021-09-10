66759e0759d871098a4eeaf5bdcc960a9af3a67d
Andreas Gunnarsson <andreas.gunnarsson@gmail.com>
2021-09-10 15:43:01 +0200

    - Made the admin component more compact; will make it easier to add validation logic if we can avoid the same logic in two different places.

--- src/page/AdminPage.tsx
+++ src/page/AdminPage.tsx
@@ -31,8 +31,8 @@ const useStyles = makeStyles((theme: Theme) =>
 
  const AdminPage: React.FC = () => {
   const classes = useStyles();
-  const [ open, setOpen ] = useState(false);
-  const [ selectedProduct, setSelectedProduct ] = useState<Product>({
+
+  const defaultProduct: Product = {
     id: -1,
     name: '',
     year: -1,
@@ -41,8 +41,11 @@ const useStyles = makeStyles((theme: Theme) =>
     price: -1,
     description: '',
     imageUrl: ''
-  });
-  const { products, deleteProduct, AddOrUpdateProduct} = useContext(ProductContext);
+  }
+
+  const [ open, setOpen ] = useState(false);
+  const [ selectedProduct, setSelectedProduct ] = useState<Product>(defaultProduct);
+  const { products, deleteProduct, AddOrUpdateProduct } = useContext(ProductContext);
   const { register, handleSubmit, unregister } = useForm<Product>();
 
   const onSubmit: SubmitHandler<Product> = (data) => {
@@ -61,63 +64,33 @@ const useStyles = makeStyles((theme: Theme) =>
 
   const handleAddNew = () => {
     unregister(['id', 'name', 'year', 'genre', 'rating', 'price', 'description', 'imageUrl']);
-    setSelectedProduct({
-      id: -1,
-      name: '',
-      year: -1,
-      genre: '',
-      rating: -1,
-      price: -1,
-      description: '',
-      imageUrl: ''
-    });
+    setSelectedProduct(defaultProduct);
     setOpen(true);
   };
 
-  const modalBodyEditProduct = (
+  const modalBody = (
   <div className={classes.modal}>
-    <h1>Edit</h1>
-      { <form onSubmit={handleSubmit(onSubmit)} style={{display: "flex", flexDirection: "row"}}>
-          <div style={{display: "flex", flexDirection: "column"}}>
-          <fieldset disabled>
-            <TextField type="number" label="Id" defaultValue={selectedProduct.id} {...register("id")} />
-          </fieldset>
-            <TextField type="text" label="Name" defaultValue={selectedProduct.name} {...register("name")} />
-            <TextField type="number" label="Year" defaultValue={selectedProduct.year} {...register("year")}/>
-            <TextField type="text" label="Genre" defaultValue={selectedProduct.genre} {...register("genre")} />
-          </div>
-          <div style={{display: "flex", flexDirection: "column"}}>
-            <TextField type="number" label="Rating" defaultValue={selectedProduct.rating} {...register("rating")} />
-            <TextField type="number" label="Price" defaultValue={selectedProduct.price} {...register("price")} />
-            <TextField type="text" label="Description" defaultValue={selectedProduct.description} {...register("description")} />
-            <TextField type="url" label="Image URL" defaultValue={selectedProduct.imageUrl} {...register("imageUrl")} />
-          </div>
-          <Button variant="contained" color="primary" type="submit">
-            Submit
-          </Button>
-      </form> }
-  </div>
-  );
-
-  const modalBodyNewProduct = (
-  <div className={classes.modal}>
-    <h1>Create new</h1>
-      { <form onSubmit={handleSubmit(onSubmit)} style={{display: "flex", flexDirection: "row"}}>
-          <div style={{display: "flex", flexDirection: "column"}}>
-            <TextField type="text" label="Name" {...register("name")} />
-            <TextField type="number" label="Year" {...register("year")}/>
-            <TextField type="text" label="Genre" {...register("genre")} />
-          </div>
-          <div style={{display: "flex", flexDirection: "column"}}>
-            <TextField type="number" label="Rating" {...register("rating")} />
-            <TextField type="number" label="Price" {...register("price")} />
-            <TextField type="text" label="Description" {...register("description")} />
-            <TextField type="url" label="Image URL" {...register("imageUrl")} />
-          </div>
-          <Button variant="contained" color="primary" type="submit">
-            Submit
-          </Button>
-      </form> }
+    { selectedProduct.id > 0 ? <h1>Edit</h1> : <h1>Create New</h1> }
+    { <form onSubmit={handleSubmit(onSubmit)} style={{display: "flex", flexDirection: "row"}}>
+        <div style={{display: "flex", flexDirection: "column"}}>
+          { selectedProduct.id > 0 &&
+            <fieldset disabled>
+              <TextField type="number" label="Id" defaultValue={selectedProduct.id} {...register("id")} />
+            </fieldset> }
+          <TextField type="text" label="Name" defaultValue={selectedProduct.name} {...register("name")} />
+          <TextField type="number" label="Year" defaultValue={selectedProduct.year} {...register("year")}/>
+          <TextField type="text" label="Genre" defaultValue={selectedProduct.genre} {...register("genre")} />
+        </div>
+        <div style={{display: "flex", flexDirection: "column"}}>
+          <TextField type="number" label="Rating" defaultValue={selectedProduct.rating} {...register("rating")} />
+          <TextField type="number" label="Price" defaultValue={selectedProduct.price} {...register("price")} />
+          <TextField type="text" label="Description" defaultValue={selectedProduct.description} {...register("description")} />
+          <TextField type="url" label="Image URL" defaultValue={selectedProduct.imageUrl} {...register("imageUrl")} />
+        </div>
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+    </form> }
   </div>
   );
 
@@ -138,9 +111,10 @@ const useStyles = makeStyles((theme: Theme) =>
         <Button onClick={handleAddNew}>Add new</Button>
       </List>
       <Modal open={open} onClose={() => {setOpen(false)}}>
-        {selectedProduct.id > 0 ? modalBodyEditProduct : modalBodyNewProduct}
+        {modalBody}
       </Modal>
     </div>
   );
 };
+
 export default AdminPage;
