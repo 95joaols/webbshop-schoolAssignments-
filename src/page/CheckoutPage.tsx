ddd75734acc73984d8b237451f5eca107b8926d4
RikardF <ri.frib@gmail.com>
2021-09-14 16:12:52 +0200

    Included validationErrors in custumerContext instead of passing by props

--- src/page/CheckoutPage.tsx
+++ src/page/CheckoutPage.tsx
@@ -1,23 +1,22 @@
 import Grid from "@material-ui/core/Grid";
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { customerErrors } from "../entity/CustomerValidation";
 import ShoppingCartTable from "../components/ShoppingCartTable";
 import CustomerInput from "../components/CustomerInput";
 import ErrorBoundary from "../components/ErrorBoundary";
-import { CustomerErrors } from "../entity/CustomerErrors";
+import { CustomerContext } from "../contexts/CustomerContext";
 
 const CheckoutPage: React.FC = () => {
   const [disabled, setDisabled] = useState<boolean>(true);
   const [totalPrice, setTotalPrice] = useState<number>(0);
-  const [validationErrors, setValidationErrors] = useState<CustomerErrors>(customerErrors);
+  const { validationErrors } = useContext(CustomerContext);
 
   useEffect(() => {
     disableIfError();
-  })
+  });
 
   function disableIfError() {
     if (Object.values(validationErrors).includes(true) || totalPrice === 0) {
@@ -27,7 +26,7 @@ const CheckoutPage: React.FC = () => {
 
   const useStyles = makeStyles((theme) => ({
     root: {
-      marginTop: 170
+      marginTop: 170,
     },
     buttonCell: {
       display: "flex",
@@ -58,15 +57,7 @@ const CheckoutPage: React.FC = () => {
           }}
         />
       </ErrorBoundary>
-      <CustomerInput
-        onSetCustomer={() => {
-          disableIfError();
-        }}
-        validationErrors={validationErrors}
-        onSetValidationError={(value: React.SetStateAction<CustomerErrors>) => {
-          setValidationErrors(value);
-        }}
-      />
+      <CustomerInput/>
       <Grid item xs={12} className={classes.buttonCell}>
         <Button
           className={classes.button}
@@ -74,9 +65,6 @@ const CheckoutPage: React.FC = () => {
           to="/summary"
           variant="contained"
           color="primary"
-          onClick={() => {
-            setValidationErrors(customerErrors);
-          }}
           disabled={disabled}
         >
           Skicka
